3615bb600834d0679356d10f83d5ace62becf33e
Nicolas Castanha <nicolascastanha2002@gmail.com>
2025-07-10 23:29:56 -0300

    network

--- build/docker/local/01_insert.sql
+++ build/docker/local/01_insert.sql
@@ -1,7 +1,7 @@
 -- Populando tb_user
 INSERT INTO tb_user (id, email, password_hash)
 VALUES 
-  ('51b00f47-5293-4100-8a24-0c5349b5ff47', 'pedrinho@email.com', '19f45c84bc9877f6bdd7b1c3c6c124355283126f');
+  ('51b00f47-5293-4100-8a24-0c5349b5ff47', 'nicolascastanha2002@gmail.com', '19f45c84bc9877f6bdd7b1c3c6c124355283126f');
 
 -- Populando tb_video_jobs
 INSERT INTO tb_video_jobs (id, user_id, status, video_path, output_path)
