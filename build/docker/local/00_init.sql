0c05539d4d42cdfe947eba088b5be0bf5cb63928
Nicolas Castanha <nicolascastanha2002@gmail.com>
2025-07-11 00:42:38 -0300

    fix

--- build/docker/local/00_init.sql
+++ build/docker/local/00_init.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS tb_user (
     id uuid PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT now()
 );
 
